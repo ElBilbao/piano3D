12924674e465762ede3bc5696000305c9f02eca0
Rodrigo Bilbao <rbilbao98@gmail.com>
2022-01-20 18:28:34 -0600

    Main stage figures

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.z = 5;
+camera.position.z = 10;
 
 var renderer = new THREE.WebGLRenderer({ antiaalias: true });
 renderer.setClearColor("#e5e5e5");
@@ -26,15 +26,41 @@ window.addEventListener("resize", () => {
 });
 
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.update();
 const loader = new GLTFLoader();
 
+// ---- PIANO
 loader.load(
   "assets/models/piano3d.glb",
   function (gltf) {
     console.log(gltf);
     scene.add(gltf.scene);
     gltf.scene.position.y = -4;
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
+
+// ---- CURTAINS
+loader.load(
+  "assets/models/curtains.glb",
+  function (curtains) {
+    console.log(curtains);
+    scene.add(curtains.scene);
+    curtains.scene.scale.set(5, 5, 1);
+    curtains.scene.position.set(-10, -4, 4);
+    curtains.scene.children[0].material.color.setHex(0x8b0000);
+
+    var curtainsRight = curtains.scene.clone();
+    scene.add(curtainsRight);
+    curtainsRight.translateX(20);
+
+    var curtainsTop = curtains.scene.clone();
+    scene.add(curtainsTop);
+    curtainsTop.position.set(0, 0, 0);
+    curtainsTop.scale.set(8, 0.8, 2);
+    curtainsTop.position.set(0, 12, 3.5);
   },
   undefined,
   function (error) {
@@ -52,7 +78,7 @@ var light = new THREE.PointLight(0xffffff, 2, 1000);
 light.position.set(10, 0, 25);
 scene.add(light);
 
-// Wood Platform
+// ---- WOOD PLATFORM
 var platGeo = new THREE.BoxGeometry(1, 1, 1);
 var woodTexture = new THREE.TextureLoader().load("assets/textures/wood.jpeg");
 woodTexture.wrapS = THREE.RepeatWrapping;
@@ -65,13 +91,46 @@ var platFace = new THREE.MeshBasicMaterial({
 var platMatCube = [platFace, platFace, platFace, platFace, platFace, platFace];
 var platform = new THREE.Mesh(platGeo, platMatCube);
 scene.add(platform);
-platform.position.y = -4.5;
-platform.translateZ(-5);
+
 platform.scale.set(30, 1, 20);
+platform.position.set(0, -4.5, -5);
+
+// ---- WALLS
+// LEFT
+var leftWallGeo = new THREE.BoxGeometry(1, 1, 1);
+var leftWallMat = new THREE.MeshBasicMaterial({ color: "black" });
+var leftWall = new THREE.Mesh(leftWallGeo, leftWallMat);
+scene.add(leftWall);
+leftWall.scale.set(1, 20, 20);
+leftWall.position.set(-15, 5, -5);
+// RIGHT
+var rightWallGeo = new THREE.BoxGeometry(1, 1, 1);
+var rightWallMat = new THREE.MeshBasicMaterial({ color: "black" });
+var rightWall = new THREE.Mesh(rightWallGeo, rightWallMat);
+scene.add(rightWall);
+rightWall.scale.set(1, 20, 20);
+rightWall.position.set(15, 5, -5);
+// BACK
+var backWallGeo = new THREE.BoxGeometry(1, 1, 1);
+var backWallMat = new THREE.MeshBasicMaterial({ color: "black" });
+var backWall = new THREE.Mesh(backWallGeo, backWallMat);
+scene.add(backWall);
+backWall.scale.set(30, 20, 1);
+backWall.position.set(0, 5, -15);
+
+// ---- ROOF
+var roofGeo = new THREE.BoxGeometry(1, 1, 1);
+var roofWallMat = new THREE.MeshBasicMaterial({ color: "black" });
+var roof = new THREE.Mesh(roofGeo, roofWallMat);
+scene.add(roof);
+roof.scale.set(30, 1, 20);
+roof.position.set(0, 15, -5);
+
+// ---- STAGE LIGHT using external three js library
 
 renderer.render(scene, camera);
 
-// RENDERING FUNCTION
+// ---- RENDERING FUNCTION
 var render = function () {
   requestAnimationFrame(render);
   controls.update();
